04c0d9d0a46d0879b203d3534be8478ea5ea7d96
Любомир Лалов <ldlalov@hotmail.com>
2022-03-12 09:08:36 +0200

    Add project files.

---
+++ 01. Extract Person Information/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace _01._Extract_Person_Information
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < inputs; i++)
+            {
+                string input = Console.ReadLine();
+                int nameStart = input.IndexOf("@")+1;
+                int nameEnd = input.IndexOf("|");
+                string name = input.Substring(nameStart, nameEnd-nameStart);
+                int ageStart = input.IndexOf("#")+1;
+                int ageEnd = input.IndexOf("*");
+                string age = input.Substring(ageStart, ageEnd - ageStart);
+                Console.WriteLine($"{ name} is { age } years old.");
+            }
+        }
+    }
+}
---
+++ 02. Ascii Sumator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02._Ascii_Sumator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int begin = char.Parse(Console.ReadLine());
+            int end = char.Parse(Console.ReadLine());
+            char[] chars = Console.ReadLine().ToCharArray();
+            int sum = 0;
+            foreach (char item in chars)
+            {
+                if (item>begin && item<end)
+                {
+                    sum += item;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
